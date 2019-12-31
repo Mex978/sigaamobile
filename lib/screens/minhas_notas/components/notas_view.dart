3a939130998ef1832d1f9d99ec5e8c5df8533cfc
Max Lima <max.lima2@gmail.com>
2019-12-31 14:14:23 -0300

    Ver notas

---
+++ lib/screens/minhas_notas/components/notas_view.dart
@@ -0,0 +1,95 @@
+import 'package:flutter/material.dart';
+import 'package:sigaamobile/shared/utils.dart';
+
+class NotasView extends StatefulWidget {
+  final List<dynamic> data;
+
+  @override
+  _NotasViewState createState() => _NotasViewState();
+
+  NotasView({this.data});
+}
+
+class _NotasViewState extends State<NotasView>
+    with SingleTickerProviderStateMixin {
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: widget.data.length,
+      child: Scaffold(
+          appBar: AppBar(
+            title: Text("Minhas Notas"),
+            bottom: TabBar(
+              isScrollable: true,
+              tabs: widget.data.map((item) {
+                return Tab(
+                  child: Text(item["periodo"].toString()),
+                );
+              }).toList(),
+            ),
+          ),
+          body: TabBarView(
+            children: widget.data.map((item) {
+              return ListView.builder(
+                itemCount: item["disciplinas"].length,
+                itemBuilder: (context, index) {
+                  var disciplina = item['disciplinas'][index];
+                  return ExpansionTile(
+                    backgroundColor: disciplina["situacao"] != null
+                        ? disciplina["situacao"] == "AM"
+                            ? Color.fromRGBO(119, 221, 119, 0.4)
+                            : Color.fromRGBO(255, 0, 0, 0.6)
+                        : Color.fromRGBO(0, 0, 0, 0.4),
+                    title: Text("${disciplina["disciplina"]}"),
+                    subtitle: Text(
+                      "${disciplina["codigo"]}",
+                      style: TextStyle(color: Color.fromRGBO(0, 0, 0, 0.4)),
+                    ),
+                    children: disciplina.keys.map<Widget>((key) {
+                      String labelName;
+                      if (key == "prova_final") {
+                        labelName = "EF";
+                      } else if (key.contains("unidade")) {
+                        labelName = "Nota " + key[key.length - 1];
+                      } else {
+                        labelName = capitalize(key);
+                      }
+
+                      if (key == "codigo" || key == "disciplina") {
+                        return Container();
+                      } else {
+                        return ListTile(
+                          title: Text(labelName),
+                          subtitle: disciplina[key] != null
+                              ? (key.toString().contains("unidade") ||
+                                      key.toString().contains("resultado") ||
+                                      key.toString().contains("prova_final"))
+                                  ? double.parse(disciplina[key]
+                                              .toString()
+                                              .replaceAll(",", ".")) >=
+                                          7.0
+                                      ? Text(
+                                          disciplina[key],
+                                          style: TextStyle(
+                                              color: Colors.green,
+                                              fontWeight: FontWeight.bold),
+                                        )
+                                      : Text(
+                                          disciplina[key],
+                                          style: TextStyle(
+                                              color: Colors.red,
+                                              fontWeight: FontWeight.bold),
+                                        )
+                                  : Text(disciplina[key])
+                              : Container(),
+                        );
+                      }
+                    }).toList(),
+                  );
+                },
+              );
+            }).toList(),
+          )),
+    );
+  }
+}
--- lib/screens/minhas_notas/minhas_notas_screen.dart
+++ lib/screens/minhas_notas/minhas_notas_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:sigaamobile/index.dart';
 import 'package:sigaamobile/screens/login/index.dart';
+import 'package:sigaamobile/screens/minhas_notas/components/notas_view.dart';
 import 'package:sigaamobile/screens/minhas_notas/minhas_notas_bloc.dart';
 import 'package:sigaamobile/shared/utils.dart';
 
@@ -10,97 +11,39 @@ class MinhasNotasScreen extends StatelessWidget {
     MinhasNotasBloc _minhasNotasBloc = BlocProvider.getBloc<MinhasNotasBloc>();
     _minhasNotasBloc.recuperarNotas();
 
-    return Scaffold(
-        appBar: AppBar(
-          title: Text("Minhas Notas"),
-        ),
-        body: StreamBuilder<RequestState>(
-          initialData: RequestState.NONE,
-          stream: _minhasNotasBloc.outStateMinhasNotas,
-          builder: (context, snapshot) {
-            if (snapshot.data == RequestState.NONE ||
-                snapshot.data == RequestState.LOADING) {
-              return Center(
-                child: CircularProgressIndicator(),
-              );
-            }
+    return StreamBuilder<RequestState>(
+      initialData: RequestState.NONE,
+      stream: _minhasNotasBloc.outStateMinhasNotas,
+      builder: (context, snapshot) {
+        if (snapshot.data == RequestState.NONE ||
+            snapshot.data == RequestState.LOADING) {
+          return Scaffold(
+            appBar: AppBar(
+              title: Text("Minhas Notas"),
+            ),
+            body: Center(
+              child: CircularProgressIndicator(),
+            ),
+          );
+        }
 
-            return StreamBuilder<List<dynamic>>(
-              initialData: [],
-              stream: _minhasNotasBloc.outMinhasNotas,
-              builder: (context, snapshot) {
-                if (snapshot == null || !snapshot.hasData) {
-                  return Center(
+        return StreamBuilder<List<dynamic>>(
+            initialData: [],
+            stream: _minhasNotasBloc.outMinhasNotas,
+            builder: (context, snapshot) {
+              if (snapshot == null || !snapshot.hasData) {
+                return Scaffold(
+                  appBar: AppBar(
+                    title: Text("Minhas Notas"),
+                  ),
+                  body: Center(
                     child: CircularProgressIndicator(),
-                  );
-                }
-
-                return ListView.separated(
-                  separatorBuilder: (context, index) {
-                    return Divider(
-                      color: Theme.of(context).primaryColor,
-                      height: 0,
-                    );
-                  },
-                  itemCount: snapshot.data.length,
-                  itemBuilder: (context, index) {
-                    var item = snapshot.data[index];
-                    return Column(
-                      children: <Widget>[
-                        Container(
-                          color: Theme.of(context).primaryColor,
-                          alignment: Alignment.centerLeft,
-                          padding: EdgeInsets.only(left: 16, top: 5, bottom: 5),
-                          child: Text(
-                            "Período ${item["periodo"]}",
-                            style: TextStyle(
-                                color: Colors.white,
-                                fontWeight: FontWeight.bold),
-                          ),
-                        ),
-                        SingleChildScrollView(
-                          scrollDirection: Axis.horizontal,
-                          child: DataTable(
-                            columnSpacing: 0,
-                            columns: item["disciplinas"][0]
-                                .keys
-                                .map<DataColumn>((key) {
-                              String labelName;
-                              if (key == "prova_final") {
-                                labelName = "EF";
-                              } else if (key.contains("unidade")) {
-                                labelName = "U" + key[key.length - 1];
-                              } else {
-                                labelName = capitalize(key);
-                              }
-                              if (key != "codigo") {
-                                return DataColumn(label: Text(labelName));
-                              } else {
-                                return DataColumn(label: Container());
-                              }
-                            }).toList(),
-                            rows:
-                                item["disciplinas"].map<DataRow>((disciplina) {
-                              print(disciplina is Map);
-                              return DataRow(
-                                  cells: disciplina.keys.map<DataCell>((key) {
-                                print(key);
-                                if (key == "codigo" ||
-                                    disciplina[key] == null) {
-                                  return DataCell(Container());
-                                }
-                                return DataCell(Text(disciplina[key]));
-                              }).toList());
-                            }).toList(),
-                          ),
-                        )
-                      ],
-                    );
-                  },
+                  ),
                 );
-              },
-            );
-          },
-        ));
+              }
+              return NotasView(data: snapshot.data);
+            });
+      },
+    );
   }
 }
